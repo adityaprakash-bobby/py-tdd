21358ab8b7a445cab308233454ee06676812199b
Aditya <adityaprakash.bobby@gmail.com>
2019-05-27 00:24:40 +0530

    Resolve error in auto deployment - Python version error

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -17,7 +17,7 @@ def _get_latest_source():
 def _update_virtualenv():
 
     if not exists('virtualenv/bin/pip'):
-        run(f'python -m venv virtualenv')
+        run(f'python3 -m venv virtualenv')
 
     run('./virtualenv/bin/pip install -r requirements.txt')
 

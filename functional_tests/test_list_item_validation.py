16b2c515769e76b68f34ae93e14ba7798ab8b27d
Aditya <adityaprakash.bobby@gmail.com>
2019-06-04 02:48:26 +0530

    add FTs to test the new javascript for Item validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,9 @@ from .base import FunctionalTest
 import time
 
 class ListItemValidationTest(FunctionalTest):
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
     
     def test_cannot_add_empty_lists(self):
 
@@ -60,6 +63,31 @@ class ListItemValidationTest(FunctionalTest):
         
         # He gets an helpful error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
+        ))
+    
+    def test_error_messages_are_cleared_on_input(self):
+
+        # Bob visits the home page and starts a new list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy hummus')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_a_item_present_in_the_list('1: Buy hummus')
+
+        # She accidentally tries to add another item which is already presesnt
+        # in the list
+        self.get_item_input_box().send_keys('Buy hummus')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        
+        # He gets an helpful error message
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # He starts typing in the input box again and sees the error message 
+        # disappear
+        self.get_item_input_box().send_keys('a')
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))

73cfdfe64c44e21c8adfa2e7a60b2ed42333f04e
Aditya <adityaprakash.bobby@gmail.com>
2019-06-08 00:33:21 +0530

    Modify the views to pass unit tests, and change the custom user model and token model

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.core.mail import send_mail
+from django.contrib import messages, auth
+from accounts.models import Token
 
 # Create your views here.
+def send_login_email(request):
+
+    email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + "?token=" + str(token.uid)
+    )
+    send_mail(
+        'Your login link for Superlists',
+        f'Use this link to log in:\n\n{url}',
+        'noreply@superlists',
+        [email]
+    )
+
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in." 
+    )
+
+    return redirect('/')
+
+def login(request):
+    
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
+        auth.login(request, user)
+    
+    return redirect('/')

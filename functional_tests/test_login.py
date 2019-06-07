c24dfd5c29b362b064a2adf275072f5e7bd99fcb
Aditya <adityaprakash.bobby@gmail.com>
2019-06-08 01:09:42 +0530

    add FTs for logging out; add supporting logout view, url and button

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -48,3 +48,14 @@ class LoginTest(FunctionalTest):
 
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(TEST_EMAIL, navbar.text)
+
+        # He now logs out of it
+        self.browser.find_element_by_link_text('Log out').click()
+
+        # He is logged out
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(TEST_EMAIL, navbar.text)

7e11b4a3dd6dd41cc3ee06793831a26b28a16d12
Aditya <adityaprakash.bobby@gmail.com>
2019-06-10 02:23:38 +0530

    Add POP3 mail helper for server-side tests

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -35,9 +35,9 @@ class FunctionalTest(StaticLiveServerTestCase):
         # to visit the site
         self.browser = webdriver.Firefox()
         
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
 
     def tearDown(self):
         # Gracefully, quits the browser 
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,19 +1,61 @@
+import re
+import os
+import time
+import poplib
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-import re
 
 from .base import FunctionalTest
 
-TEST_EMAIL = 'bob@example.com'
 SUBJECT = 'Your login link for Superlists'
 
 class LoginTest(FunctionalTest):
 
+    def wait_for_mail(self, test_email, subject):
+
+        if not self.staging_server:
+
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+        
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.mail.yahoo.com')
+
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ.get('YAHOO_PASSWORD'))
+
+            while (time.time() - start) < 60:
+                # get 10 newest messages
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting message', i)
+                    _, lines, _ =  inbox.retr(i)
+                    lines = [l.decode('utf-8') for l in lines]
+                 
+                    if f'Subject: {subject}' in lines:
+                        email_id = 1
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
+
     def test_can_get_email_link_to_log_in(self):
         # Bob goes to the website and finds there's a new feature to log in.
         # He is prompted to enter the email and so does he do.
+        if self.staging_server:
+            test_email = 'testinggoatisthebest@yahoo.com'
+        else:
+            test_email = 'bob@example.com'
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(test_email)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         # A message appears that a email has been sent to him with the log in
@@ -24,16 +66,14 @@ class LoginTest(FunctionalTest):
         ))
 
         # She checks her mail and finds a message
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_mail(test_email, SUBJECT)
 
         # It has a a URL link in it
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'http://.+/.+$', body)
 
         if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
+            self.fail(f'Could not find url in email body:\n{body}')
         
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
@@ -42,10 +82,10 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # He is logged in!
-        self.wait_to_be_logged_in(TEST_EMAIL) 
+        self.wait_to_be_logged_in(test_email) 
 
         # He now logs out of it
         self.browser.find_element_by_link_text('Log out').click()
 
         # He is logged out
-        self.wait_to_be_logged_out(TEST_EMAIL)
+        self.wait_to_be_logged_out(test_email)

4309a0f715a39914549249a59be555e79ed9d163
Aditya <adityaprakash.bobby@gmail.com>
2019-06-13 22:48:15 +0530

    update base template; check if my_lists uses correct template

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -208,3 +208,7 @@ class NewListTest(TestCase):
         _response = self.client.post('/lists/new', data={'text':''})
         self.assertIsInstance(_response.context['form'], ItemForm)
 
+class MyListsTest(TestCase):
+    def test_my_lists_url_renders_my_lists_template(self):
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertTemplateUsed(response, 'my_lists.html')
